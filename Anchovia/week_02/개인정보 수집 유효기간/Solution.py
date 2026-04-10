03733de129b473f2e3303486f671daee265d5634
Anchovia <hodolye@naver.com>
2026-04-11 04:10:41 +0900

    [PGS] 개인정보 수집 유효기간 / 전병훈

---
+++ Anchovia/week_02/개인정보 수집 유효기간/Solution.py
@@ -0,0 +1,22 @@
+def solution(today, terms, privacies):
+    answer = []
+    def change_days(date):
+        y, m, d = map(int, date.split("."))
+        return y * 12 * 28 + m * 28 + d
+    
+    term_dict = {}
+    for term in terms:
+        name, month = term.split()
+        term_dict[name] = int(month)
+
+    today_days = change_days(today)
+    for i, priv in enumerate(privacies):
+        date, term = priv.split()
+
+        start_days = change_days(date)
+        expire_days = start_days + term_dict[term] * 28
+
+        if expire_days <= today_days:
+            answer.append(i + 1)
+
+    return answer
