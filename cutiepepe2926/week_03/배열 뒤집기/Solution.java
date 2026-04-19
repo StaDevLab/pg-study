1747a2d9241bd919aa02b2070cae1586923b38c4
cutiepepe29266 <cutiepepe2926@gmail.com>
2026-04-19 19:59:31 +0900

    [PGS] 배열 뒤집기 / 이건희

---
+++ cutiepepe2926/week_03/배열 뒤집기/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+
+        int[] answer = new int[num_list.length];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int nl : num_list) {
+            stack.push(nl);
+        }
+
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = stack.pop();
+        }
+
+        return answer;
+
+    }
+}
