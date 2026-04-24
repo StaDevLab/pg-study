927d13b67fe84809f3da51251d676aca08658336
cutiepepe2926 <cutiepepe2926@gmail.com>
2026-04-24 15:34:56 +0900

    [PGS] 특이한 정렬 / 이건희

---
+++ cutiepepe2926/week_04/특이한 정렬/Solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+
+        int[] answer = new int[numlist.length];
+
+        Integer[] temp = new Integer[numlist.length];
+
+        int idx = 0;
+
+        for (Integer t : numlist) {
+            temp[idx++] = t;
+        }
+
+        Arrays.sort(temp, (a,b) -> {
+
+            int absA = Math.abs(n-a);
+            int absB = Math.abs(n-b);
+
+            if (absA != absB) {
+                return Integer.compare(absA, absB);
+            }
+            return Integer.compare(b, a);
+        });
+
+        idx = 0;
+
+        for (int t : temp) {
+            answer[idx++] = t;
+        }
+
+        return answer;
+
+    }
+}
