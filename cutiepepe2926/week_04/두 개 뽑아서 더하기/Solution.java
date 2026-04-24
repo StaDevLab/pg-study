d69de15dd3ca5a424818925b52493d89c8469501
cutiepepe2926 <cutiepepe2926@gmail.com>
2026-04-24 15:35:49 +0900

    [PGS] 두 개 뽑아서 더하기 / 이건희

---
+++ cutiepepe2926/week_04/두 개 뽑아서 더하기/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+
+        HashSet<Integer> temp = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i == j) continue;
+                temp.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        int[] answer = new int[temp.size()];
+
+        int idx = 0;
+
+        for (int r : temp) {
+            answer[idx++] = r;
+        }
+
+        Arrays.sort(answer);
+
+        return answer;
+
+    }
+}
