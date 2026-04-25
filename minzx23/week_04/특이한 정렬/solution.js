09dc0047f8117e79fc5c5bbc79f11ad330a82f7c
minzx23 <kmjn0623@gmail.com>
2026-04-25 20:52:19 +0900

    [PGS] 특이한 정렬 / 강민주

---
+++ minzx23/week_04/특이한 정렬/solution.js
@@ -0,0 +1,13 @@
+function solution(numlist, n) {
+    return numlist.sort((a,b)=>{
+        const A= Math.abs(a-n);
+        const B= Math.abs(b-n);
+        
+        if (A===B){
+            return b-a;
+        }
+        else{
+            return A-B;
+        }
+    })
+}
