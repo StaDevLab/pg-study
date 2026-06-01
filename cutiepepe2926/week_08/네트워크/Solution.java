4595d015bad6dd461c2193a62a12d01d31322470
cutiepepe29266 <cutiepepe2926@gmail.com>
2026-06-01 20:09:55 +0900

    [PGS] 네트워크 / 이건희

---
+++ cutiepepe2926/week_08/네트워크/Solution.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    static boolean[] visited; // 방문체크용
+    static int[][] network;
+
+    public int solution(int n, int[][] computers) {
+
+        int answer = 0;
+        visited = new boolean[n];
+        network = computers;
+
+        // 각 컴퓨터를 순회
+        for (int i = 0; i < n; i++) {
+
+            // 이미 방문했다면 무시
+            if (!visited[i]) {
+
+                // dfs 순회로 연결된 네트워크 탐지
+                dfs(i);
+
+                answer++;
+            }
+
+        }
+
+        return answer;
+
+    }
+
+    static void dfs(int node) {
+
+        // 해당 노드 방문 체크
+        visited[node] = true;
+
+        // 연결된 만큼 방문 + 방문여부 체크
+        for (int i = 0; i < network.length; i++) {
+            if (network[node][i] == 1 && !visited[i]) {
+                dfs(i);
+            }
+        }
+
+    }
+
+}
